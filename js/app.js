fd015774cb67e7442cec745f653cfe47f86aa78f
Carmeshia <65563803+Meshia13@users.noreply.github.com>
2024-05-28 19:28:13 -0400

    adding content to sections

---
+++ js/app.js
@@ -0,0 +1,7 @@
+
+// const menuItems = ["Home", "About", "Movies", "Subscribe"];
+
+// Accessing the element
+const menuLink = document.getElementById("nav_list");
+console.log(menuLink.innerHTML);
+
