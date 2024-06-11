d9d396b3394e5b7c9db6232aa8b4bb004a859f50
Carmeshia <65563803+Meshia13@users.noreply.github.com>
2024-06-10 21:17:03 -0400

    docs: updated html and js file

--- js/app.js
+++ js/app.js
@@ -2,6 +2,40 @@
 // const menuItems = ["Home", "About", "Movies", "Subscribe"];
 
 // Accessing the element
-const menuLink = document.getElementById("nav_list");
-console.log(menuLink.innerHTML);
+/**
+ * Define Global Variables
+ * 
+*/
 
+// Getting the <ul> element and assigning to variable
+const myNav = document.getElementById("nav_list");
+
+// Selecting all sections
+const sections = document.querySelectorAll("section");
+
+// build the nav
+
+// This function builds the navigation bar dynamically
+function buildNavbar() {
+    for(const section of sections ) {
+        // creating anchor elements for the 'a href' in order to scroll to that section
+        const anchor = document.createElement("a");
+        // creating <li> for the ul list 
+        const li = document.createElement("li");
+        // adding class name to elements
+        li.classList.add("menu_list");
+        // using the 'id" attributes from section tags to anchor them to the 'href'
+        anchor.href = `#${section.id}`;
+        anchor.textContent = section.dataset.nav;
+
+        // clicking an item from the navigation menu will scroll the section into view
+        anchor.addEventListener("click", function(clickEvent) {
+            section.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"})
+        });
+
+        // appending acnchor to li & li to myNav(parent element)
+        li.appendChild(anchor);
+        myNav.appendChild(li);
+    }
+}
+buildNavbar()
